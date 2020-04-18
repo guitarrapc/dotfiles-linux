f2657a176bc63e791e15307a43cfe48132916bf6
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-04-19 05:54:29 +0900

    chore: add comment

--- ROOT/etc/profile.d/profile_ansible_init_docker.sh
+++ ROOT/etc/profile.d/profile_ansible_init_docker.sh
@@ -1,5 +1,5 @@
 sudo -S cgroupfs-mount
 sudo usermod -aG docker $USER
 sudo service docker start
-# using windows docker-compose
+# wsl1 using windows docker-compose. wsl2 don't need this line.
 export DOCKER_HOST=tcp://localhost:2375
