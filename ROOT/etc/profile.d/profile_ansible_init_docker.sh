db47ddf5c1ba8400acb502b256c8a03bbcb5749b
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-05-29 16:47:12 +0900

    chore: remove docker profile

--- ROOT/etc/profile.d/profile_ansible_init_docker.sh
+++
@@ -1,5 +0,0 @@
-sudo -S cgroupfs-mount
-sudo usermod -aG docker $USER
-sudo service docker start
-# wsl1 using windows docker-compose. wsl2 don't need this line.
-# export DOCKER_HOST=tcp://localhost:2375
