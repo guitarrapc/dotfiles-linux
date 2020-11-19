8c899e4659dd613a9b0f06ddeeed3a078d80f893
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-11-20 04:39:54 +0900

    fix: kubectx/kubens bash completion

--- ROOT/etc/profile.d/profile_dotfiles_init_kubectx.sh
+++ ROOT/etc/profile.d/profile_dotfiles_init_kubectx.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # https://github.com/ahmetb/kubectx
 KUBECTX_PATH=${HOME}/github/kubectx
-COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
-ln -sf ${KUBECTX_PATH}/completion/kubens.bash $COMPDIR/kubens
-ln -sf ${KUBECTX_PATH}/completion/kubectx.bash $COMPDIR/kubectx
+source ${KUBECTX_PATH}/completion/kubens.bash
+source ${KUBECTX_PATH}/completion/kubectx.bash
