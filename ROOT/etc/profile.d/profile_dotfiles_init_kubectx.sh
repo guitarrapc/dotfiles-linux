e083db92b8228f6f570067dbf8574841773955d6
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-11-20 13:28:03 +0900

    fix: bash completion for kubectx/kubens should check command exists.

--- ROOT/etc/profile.d/profile_dotfiles_init_kubectx.sh
+++ ROOT/etc/profile.d/profile_dotfiles_init_kubectx.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # https://github.com/ahmetb/kubectx
 KUBECTX_PATH=${HOME}/github/kubectx
-source ${KUBECTX_PATH}/completion/kubens.bash
-source ${KUBECTX_PATH}/completion/kubectx.bash
+if command -v kubens >/dev/null 2>&1
+then
+    source ${KUBECTX_PATH}/completion/kubens.bash
+fi
+
+if command -v kubectx >/dev/null 2>&1
+then
+    KUBECTX_PATH=${HOME}/github/kubectx
+    source ${KUBECTX_PATH}/completion/kubectx.bash
+fi
