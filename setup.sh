f2dfa64593871365220bb519d75b96c8ea099507
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-09-25 21:23:58 +0900

    fix: exit handling

--- setup.sh
+++ setup.sh
@@ -136,8 +136,8 @@ print_result() {
         && print_success "$2" \
         || print_error "$2"
 
-    [ "$3" == "true" ] && [ $1 -ne 0 ] \
-        && exit
+    [ $1 -ne 0 ] \
+        && exit $1
 }
 
 print_success() {
