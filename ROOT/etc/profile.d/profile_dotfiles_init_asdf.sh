869642becccf357b6e83014985df6b5d02d7c52b
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-02-10 16:51:54 +0900

    chore: why not .bashrc_custom

--- ROOT/etc/profile.d/profile_dotfiles_init_asdf.sh
+++ ROOT/etc/profile.d/profile_dotfiles_init_asdf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# ansible can't reload from .bashrc_custom, but profile can reloaded.
 # https://asdf-vm.com/guide/getting-started.html#community-supported-download-methods
 if [[ -d "${HOME}/.asdf" ]]; then
     source "${HOME}/.asdf/asdf.sh"
