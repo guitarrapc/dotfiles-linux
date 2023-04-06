6eddb32aa08e692dcdd3b8d866868f8663b4d7e7
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-04-06 12:35:44 +0900

    feat: add aws-vault BACKEND

--- ROOT/etc/profile.d/profile_dotfiles_init_aws.sh
+++ ROOT/etc/profile.d/profile_dotfiles_init_aws.sh
@@ -4,4 +4,7 @@ if command -v aws_completer >/dev/null 2>&1; then
     complete -C "$HOME/.asdf/shims/aws_completer" aws
 fi
 
+# https://github.com/99designs/aws-vault/blob/90c6834e431769acbeb13223dcae996923b8820e/contrib/docker/Dockerfile#L4
+if command -v aws-vault >/dev/null 2>&1; then
+    export AWS_VAULT_BACKEND=file
 fi
