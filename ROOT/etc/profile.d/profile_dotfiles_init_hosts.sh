59362e9e8650b863ce3eef81753ecf5eebfda338
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2023-04-16 08:34:51 +0900

    fix: sudo, remove echo

--- ROOT/etc/profile.d/profile_dotfiles_init_hosts.sh
+++ ROOT/etc/profile.d/profile_dotfiles_init_hosts.sh
@@ -1,9 +1,6 @@
 # rewrite auto-generated /etc/hosts file's kubernetes.docker.internal to point to the host's IP
 ip=$(cat /etc/resolv.conf | grep nameserver | cut -d' ' -f2-)
-if (( $(grep -c . <<<"$ip") > 1 )); then
-    # wsl will enter this section.
-    echo skipping /etc/hosts rewrite because multiple nameservers found
-else
-    # wsl2 will enter this section.
-    sed -i "/kubernetes\.docker\.internal/ s/.*/$ip\tkubernetes\.docker\.internal/g" /etc/hosts
+if ! (( $(grep -c . <<<"$ip") > 1 )); then
+  # wsl2 will enter this section. wsl1 will not (ipv4 & ipv6 entry found).
+  sudo sed -i "/kubernetes\.docker\.internal/ s/.*/$ip\tkubernetes\.docker\.internal/g" /etc/hosts
 fi
