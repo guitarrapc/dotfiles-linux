1543441cdf334164a21405ea6268d9418766f93c
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2023-04-16 08:32:44 +0900

    fix: avoid wsl1 to update /etc/hosts

--- ROOT/etc/profile.d/profile_dotfiles_init_hosts.sh
+++ ROOT/etc/profile.d/profile_dotfiles_init_hosts.sh
@@ -1,3 +1,9 @@
 # rewrite auto-generated /etc/hosts file's kubernetes.docker.internal to point to the host's IP
 ip=$(cat /etc/resolv.conf | grep nameserver | cut -d' ' -f2-)
-sed "/kubernetes\.docker\.internal/ s/.*/$ip\tkubernetes\.docker\.internal/g" /etc/hosts
+if (( $(grep -c . <<<"$ip") > 1 )); then
+    # wsl will enter this section.
+    echo skipping /etc/hosts rewrite because multiple nameservers found
+else
+    # wsl2 will enter this section.
+    sed -i "/kubernetes\.docker\.internal/ s/.*/$ip\tkubernetes\.docker\.internal/g" /etc/hosts
+fi
