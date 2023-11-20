310f2f16754bbfca25fe933171b41cf27e02733b
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-11-20 15:02:37 +0900

    fix: wsl2 handling

--- ROOT/etc/profile.d/profile_dotfiles_init_hosts.sh
+++ ROOT/etc/profile.d/profile_dotfiles_init_hosts.sh
@@ -1,6 +1,11 @@
-# rewrite auto-generated /etc/hosts file's kubernetes.docker.internal to point to the host's IP
-ip=$(cat /etc/resolv.conf | grep nameserver | cut -d' ' -f2-)
-if ! (( $(grep -c . <<<"$ip") > 1 )); then
-  # wsl2 will enter this section. wsl1 will not (ipv4 & ipv6 entry found).
-  sudo sed -i "/kubernetes\.docker\.internal/ s/.*/$ip\tkubernetes\.docker\.internal/g" /etc/hosts
+#!/bin/bash
+is_wsl2=$(cat /proc/version | grep -c microsoft-standard)
+
+if [[ "$is_wsl2" == "1" ]]; then
+    # WSL2 should Rewrite auto-generated /etc/hosts file's kubernetes.docker.internal to point to the host's IP
+    # WSL1 don't need this.
+    ip=$(cat /etc/resolv.conf | grep nameserver | cut -d' ' -f2-)
+    if ! (( $(grep -c . <<<"$ip") > 1 )); then
+    sudo sed -i "/kubernetes\.docker\.internal/ s/.*/$ip\tkubernetes\.docker\.internal/g" /etc/hosts
+    fi
 fi
