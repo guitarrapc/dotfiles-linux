7f93c305c1712603892d2dffc33fb3fbd9de998f
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-07-26 11:38:47 +0900

    Revert "fix: dotnet export"
    This reverts commit cadf64b2ba72f51a119c1e774d5d8a21c011eaf3.

--- ROOT/etc/profile.d/profile_dotfiles_init_asdf.sh
+++ ROOT/etc/profile.d/profile_dotfiles_init_asdf.sh
@@ -2,6 +2,6 @@
 # ansible can't reload from .bashrc_custom, but profile can reloaded.
 # https://asdf-vm.com/guide/getting-started.html#community-supported-download-methods
 if [[ -d "${HOME}/.asdf" ]]; then
-  source "${HOME}/.asdf/asdf.sh"
-  source "${HOME}/.asdf/completions/asdf.bash"
+    source "${HOME}/.asdf/asdf.sh"
+    source "${HOME}/.asdf/completions/asdf.bash"
 fi
--- ROOT/etc/profile.d/profile_dotfiles_init_dotnet.sh
+++ ROOT/etc/profile.d/profile_dotfiles_init_dotnet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export DOTNET_ROOT="$HOME/.dotnet"
-export PATH="$DOTNET_ROOT:$DOTNET_ROOT/tools:$PATH"
+export DOTNET_ROOT=$HOME/.dotnet
+export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
