cadf64b2ba72f51a119c1e774d5d8a21c011eaf3
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-07-26 11:30:52 +0900

    fix: dotnet export

--- ROOT/etc/profile.d/profile_dotfiles_init_kubectx.sh
+++ ROOT/etc/profile.d/profile_dotfiles_init_kubectx.sh
@@ -2,10 +2,10 @@
 # https://github.com/ahmetb/kubectx
 KUBECTX_PATH=${HOME}/github/kubectx
 if command -v kubens >/dev/null 2>&1; then
-    source ${KUBECTX_PATH}/completion/kubens.bash
+  source ${KUBECTX_PATH}/completion/kubens.bash
 fi
 
 if command -v kubectx >/dev/null 2>&1; then
-    KUBECTX_PATH=${HOME}/github/kubectx
-    source ${KUBECTX_PATH}/completion/kubectx.bash
+  KUBECTX_PATH=${HOME}/github/kubectx
+  source ${KUBECTX_PATH}/completion/kubectx.bash
 fi
