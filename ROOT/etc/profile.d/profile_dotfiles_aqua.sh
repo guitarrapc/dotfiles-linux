5e99296df6928b9e7eec35ee4ad851f214d96cc3
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-09-02 13:37:22 +0900

    feat: add aqua global config

--- ROOT/etc/profile.d/profile_dotfiles_aqua.sh
+++ ROOT/etc/profile.d/profile_dotfiles_aqua.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 if [[ -d "${HOME}/.local/share/aquaproj-aqua/bin" ]]; then
     export PATH="${HOME}/.local/share/aquaproj-aqua/bin:$PATH"
+    export AQUA_GLOBAL_CONFIG="$HOME/.config/aquaproj-aqua/aqua.yaml"
+fi
+
+if command -v aqua &> /dev/null; then
+    # shellcheck disable=SC1090
+    source <(aqua completion bash)
 fi
