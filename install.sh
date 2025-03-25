4e765809812f6d1adb20ca85d1895c03194b503a
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-03-25 16:19:04 +0900

    ci: missing aqua.yaml

--- install.sh
+++ install.sh
@@ -161,7 +161,7 @@ function print_success {
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 # finds all .dotfiles in this folder
-declare -a FILES_TO_SYMLINK=$(cd "$SCRIPT_DIR"; find "." -maxdepth 1 -type f -name ".*" -not -name .DS_Store -not -name .git -not -name .gitignore -not -name .gitattributes -not -name .bash_history -not -name .editorconfig | sed -e 's|//|/|' | sed -e 's|./.|.|')
+declare -a FILES_TO_SYMLINK=$(cd "$SCRIPT_DIR"; find "." -maxdepth 1 -type f -name ".*" -not -name .DS_Store -not -name .git -not -name .gitignore -not -name .gitattributes -not -name .bash_history -not -name .editorconfig -not -name aqua.yaml | sed -e 's|//|/|' | sed -e 's|./.|.|')
 # declare other folders
 OTHERS="HOME ROOT"
 
