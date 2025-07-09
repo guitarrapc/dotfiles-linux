dd4dc20b03c5e960148f7993b3f01712428ebd05
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-07-09 18:59:55 +0900

    chore: create dir

--- ROOT/etc/profile.d/profile_dotfiles_init_npm.sh
+++ ROOT/etc/profile.d/profile_dotfiles_init_npm.sh
@@ -2,3 +2,7 @@
 # see: https://zenn.dev/shunsuke_suzuki/articles/aqua-nodejs-support
 export NPM_CONFIG_PREFIX=${XDG_DATA_HOME:-$HOME/.local/share}/npm-global
 export PATH=$NPM_CONFIG_PREFIX/bin:$PATH
+
+if [[ -d "$NPM_CONFIG_PREFIX/bin" ]]; then
+  mkdir -p "$NPM_CONFIG_PREFIX/bin"
+fi
