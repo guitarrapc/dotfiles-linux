1da986a44c1d2096a6bb0004e9c72ad1356aaee6
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-07-09 19:01:09 +0900

    chore; create

--- ROOT/etc/profile.d/profile_dotfiles_init_npm.sh
+++ ROOT/etc/profile.d/profile_dotfiles_init_npm.sh
@@ -3,6 +3,6 @@
 export NPM_CONFIG_PREFIX=${XDG_DATA_HOME:-$HOME/.local/share}/npm-global
 export PATH=$NPM_CONFIG_PREFIX/bin:$PATH
 
-if [[ -d "$NPM_CONFIG_PREFIX/bin" ]]; then
+if [[ ! -d "$NPM_CONFIG_PREFIX/bin" ]]; then
   mkdir -p "$NPM_CONFIG_PREFIX/bin"
 fi
