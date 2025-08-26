42e371fab1743eae9725e907a7e6bd6169ad5739
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-08-27 01:51:15 +0900

    chore: remove aqua packages to ansible

---
+++ ROOT/etc/profile.d/profile_dotfiles_init_step.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+_step_cli_bash_autocomplete() {
+        local cur opts base
+        COMPREPLY=()
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        if [[ "$cur" == "-"* ]]; then
+                opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} ${cur} --generate-bash-completion )
+        else
+                opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
+        fi
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+}
+
+if command -v step >/dev/null 2>&1; then
+  complete -o bashdefault -o default -o nospace -F _step_cli_bash_autocomplete step
+fi
